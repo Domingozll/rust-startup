279f980c0e68bd7f9b9b070b35d4b16ccf75e36d
leo <dominzl881@gmail.com>
2025-08-06 01:56:37 +0800

    moduls case modify

--- RUST-LIB/AUTH_SERVICE/src/main.rs
+++ RUST-LIB/AUTH_SERVICE/src/main.rs
@@ -1,8 +1,11 @@
 use AUTH_SERVICE::Credentials;
 
 fn main() {
+    println!("runing RUST-LIB/AUTH_SERVICE...");
     let creds = Credentials {
         username: "letsgetresty".to_owned(),
         password: "password123".to_owned(),
     };
+    println!("runing RUST-LIB/AUTH_SERVICE end...");
+
 }
