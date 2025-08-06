5d594e85c164eae2020d54500ffbc333736eeed5
leo <dominzl881@gmail.com>
2025-08-07 02:10:13 +0800

    dependency case

--- RUST-LIB/AUTH_SERVICE/src/lib.rs
+++ RUST-LIB/AUTH_SERVICE/src/lib.rs
@@ -1,4 +1,5 @@
 #![allow(dead_code, unused_variables)]
+use rand::prelude::*;
 
 mod database;
 mod auth_utils;
@@ -7,6 +8,8 @@ pub use auth_utils::models::Credentials;
 use database::Status;
 
 pub fn authenticate(creds: Credentials) {
+    let timeout = thread_rng().gen_range(100..500);
+    println!("timeout is: {timeout}");
     if let Status::Connected = database::connect_to_database() {
         auth_utils::login(creds);
     }
--- RUST-LIB/AUTH_SERVICE/src/main.rs
+++ RUST-LIB/AUTH_SERVICE/src/main.rs
@@ -6,6 +6,6 @@ fn main() {
         username: "letsgetresty".to_owned(),
         password: "password123".to_owned(),
     };
+    AUTH_SERVICE::authenticate(creds);
     println!("runing RUST-LIB/AUTH_SERVICE end...");
-
 }
